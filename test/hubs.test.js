3673144e52960c666f020f68a6d5f47a61620cf1
Michael Williamson <mike@zwobble.org>
2012-08-18 13:47:17 +0100

    Ensure that double star listener fires first

--- test/hubs.test.js
+++ test/hubs.test.js
@@ -25,6 +25,23 @@ exports.canEmitAndHandleEventWithDataOnHub = function(test) {
 exports.canUseDoubleStarToListenToAllEvents = function(test) {
     var hub = hubs.createHub();
     
+    var doubleStarFired = false;
+    
+    hub.on("projects.compiler.finished", function(firstName, lastName) {
+        test.equal(doubleStarFired, true);
+        test.done();
+    });
+    
+    hub.on("**", function() {
+        doubleStarFired = true
+    });
+    
+    hub.emit("projects.compiler.finished");
+};
+
+exports.doubleStarListenerFiresBeforeSpecificListeners = function(test) {
+    var hub = hubs.createHub();
+    
     hub.on("**", function() {
         test.done();
     });
