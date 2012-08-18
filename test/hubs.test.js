30e8daf3313dc2bb3c6292df36a28defc37338d6
Michael Williamson <mike@zwobble.org>
2012-08-18 13:23:46 +0100

    Add a very hacked together prototype of evented builds

---
+++ test/hubs.test.js
@@ -0,0 +1,23 @@
+var hubs = require("../lib/hubs");
+
+exports.canEmitAndHandleEventOnHub = function(test) {   
+    var hub = hubs.createHub();
+    
+    hub.on("name", function() {
+        test.done();
+    });
+    
+    hub.emit("name");
+};
+
+exports.canEmitAndHandleEventWithDataOnHub = function(test) {   
+    var hub = hubs.createHub();
+    
+    hub.on("name", function(firstName, lastName) {
+        test.equal(firstName, "Bob");
+        test.equal(lastName, "Bobertson");
+        test.done();
+    });
+    
+    hub.emit("name", "Bob", "Bobertson");
+};
