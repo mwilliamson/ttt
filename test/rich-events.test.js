9cb5d50bd3f4acd3d3350922bae8d06c3596b488
Michael Williamson <mike@zwobble.org>
2012-08-19 10:46:58 +0100

    Add separate rich events module

---
+++ test/rich-events.test.js
@@ -0,0 +1,72 @@
+var richEvents = require("../lib/rich-events");
+
+exports.canEmitAndHandleEvent = function(test) {
+    var emitter = new richEvents.EventEmitter();
+    
+    emitter.on("name", function() {
+        test.done();
+    });
+    
+    emitter.emit("name");
+};
+
+exports.canEmitAndHandleEventWithDataOnHub = function(test) {
+    var emitter = new richEvents.EventEmitter();
+    
+    emitter.on("name", function(firstName, lastName) {
+        test.equal(firstName, "Bob");
+        test.equal(lastName, "Bobertson");
+        test.done();
+    });
+    
+    emitter.emit("name", "Bob", "Bobertson");
+};
+
+exports.canUseDoubleStarToListenToAllEvents = function(test) {
+    var emitter = new richEvents.EventEmitter();
+    
+    var doubleStarFired = false;
+    
+    emitter.on("projects.compiler.finished", function(firstName, lastName) {
+        test.equal(doubleStarFired, true);
+        test.done();
+    });
+    
+    emitter.on("**", function() {
+        doubleStarFired = true
+    });
+    
+    emitter.emit("projects.compiler.finished");
+};
+
+exports.doubleStarListenerFiresBeforeSpecificListeners = function(test) {
+    var emitter = new richEvents.EventEmitter();
+    
+    emitter.on("**", function() {
+        test.done();
+    });
+    
+    emitter.emit("projects.compiler.finished");
+};
+
+exports.thisIsBoundToEventWithNameInListener = function(test) {
+    var emitter = new richEvents.EventEmitter();
+    
+    emitter.on("**", function() {
+        test.equal(this.name, "projects.compiler.finished");
+        test.done();
+    });
+    
+    emitter.emit("projects.compiler.finished");
+};
+
+exports.singleStarIsWildcardForEventNameSection = function(test) {
+    var emitter = new richEvents.EventEmitter();
+    
+    emitter.on("projects.*.finished", function() {
+        test.equal(this.name, "projects.compiler.finished");
+        test.done();
+    });
+    
+    emitter.emit("projects.compiler.finished");
+};
